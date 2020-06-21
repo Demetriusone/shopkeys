57a7546bd36641e0bba139db06613cacd01be63b
Demetriusone <wh46tesmoke@gmail.com>
2020-06-22 00:21:18 +0300

    0.22

--- src/js/Common.js
+++ src/js/Common.js
@@ -50,11 +50,6 @@ export class Common {
   }
 }
 
-$('ul.nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(100);
-}, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(100);
-});
 
 
 
