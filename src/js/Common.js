334a6d29e43971e47fb6eaa1310c0c93a9fc5ae7
Demetriusone <wh46tesmoke@gmail.com>
2020-06-21 19:29:06 +0300

    nav 0.1

--- src/js/Common.js
+++ src/js/Common.js
@@ -50,9 +50,9 @@ export class Common {
 }
 
 $('ul.nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(100);
 }, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(100);
 });
 
 
